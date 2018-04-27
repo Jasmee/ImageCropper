745fb9ed3fa98b208364bae8690187c9b7d83c89
Jasmee Sengupta <jasmee.sengupta@in.ibm.com>
2018-04-27 12:19:10 +0530

    Basic crop.

--- ImageCropper/InstagramViewController.swift
+++ ImageCropper/InstagramViewController.swift
@@ -4,21 +4,38 @@
 //
 //  Created by Jasmee Sengupta on 08/03/18.
 //  Copyright © 2018 Jasmee Sengupta. All rights reserved.
-// crash fix
+//  not very satisfied
 
 import UIKit
 
 class InstagramViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet{
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 1.0
+            scrollView.maximumZoomScale = 10.0
+        }
+    }
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var imageViewHeight: NSLayoutConstraint!
-    @IBOutlet weak var imageViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var cropAreaView: CropAreaView!
+    
+    var cropArea:CGRect{
+        get{
+            let factor = imageView.image!.size.width/view.frame.width
+            let scale = 1/scrollView.zoomScale
+            let imageFrame = imageView.imageFrame()
+            let x = (scrollView.contentOffset.x + cropAreaView.frame.origin.x - imageFrame.origin.x) * scale * factor
+            let y = (scrollView.contentOffset.y + cropAreaView.frame.origin.y - imageFrame.origin.y) * scale * factor
+            let width = cropAreaView.frame.size.width * scale * factor
+            let height = cropAreaView.frame.size.height * scale * factor
+            return CGRect(x: x, y: y, width: width, height: height)
+        }
+    }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
@@ -32,6 +49,8 @@ class InstagramViewController: UIViewController, UIScrollViewDelegate, UIImagePi
         return imageView
     }
     
+    // MARK: IBActions
+    
     @IBAction func openPhotoLibrary(_ sender: UIBarButtonItem) {
         let photoPicker = UIImagePickerController()
         photoPicker.delegate = self
@@ -39,39 +58,19 @@ class InstagramViewController: UIViewController, UIScrollViewDelegate, UIImagePi
         photoPicker.allowsEditing = false
         present(photoPicker, animated: true, completion: nil)
     }
-    
+
     @IBAction func cropTapped(_ sender: UIButton) {
-        crop()
+        let croppedCGImage = imageView.image?.cgImage?.cropping(to: cropArea)
+        let croppedImage = UIImage(cgImage: croppedCGImage!)
+        imageView.image = croppedImage
+        scrollView.zoomScale = 1
     }
-    
-    func setImageToCrop(image: UIImage) {
-        imageView.image = image
-        imageViewHeight.constant = image.size.height
-        imageViewWidth.constant = image.size.width
-        let scaleWidth = scrollView.frame.size.width/image.size.width
-        let scaleHeight = scrollView.frame.size.height/image.size.height
-        let maxScale = max(scaleHeight, scaleWidth)
-        scrollView.minimumZoomScale = maxScale
-        scrollView.zoomScale = maxScale
-    }
-    
-    func crop() {
-        let scale:CGFloat = 1/scrollView.zoomScale
-        
-        let x:CGFloat = scrollView.contentOffset.x * scale
-        let y:CGFloat = scrollView.contentOffset.y * scale
-        
-        let width:CGFloat = scrollView.frame.size.width * scale
-        let height:CGFloat = scrollView.frame.size.height * scale
-        
-        let croppedCGImage = imageView.image?.cgImage?.cropping(to: CGRect(x: x, y: y, width: width, height: height))
-        let croppedImage = UIImage(cgImage: croppedCGImage!) // crash
-        setImageToCrop(image: croppedImage)
-    }
+
+    // MARK: Image picker delegates
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let selectedImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
-            setImageToCrop(image: selectedImage)
+            imageView.image = selectedImage
         }
         dismiss(animated: true, completion: nil)
     }
@@ -81,4 +80,26 @@ class InstagramViewController: UIViewController, UIScrollViewDelegate, UIImagePi
     }
 }
 
-
+extension UIImageView{
+    
+    func imageFrame() -> CGRect {
+        
+        let imageViewSize = self.frame.size
+        guard let imageSize = self.image?.size else{ return CGRect.zero }
+        
+        let imageRatio = imageSize.width / imageSize.height
+        let imageViewRatio = imageViewSize.width / imageViewSize.height
+        
+        if imageRatio < imageViewRatio {
+            let scaleFactor = imageViewSize.height / imageSize.height
+            let width = imageSize.width * scaleFactor
+            let topLeftX = (imageViewSize.width - width) * 0.5
+            return CGRect(x: topLeftX, y: 0, width: width, height: imageViewSize.height)
+        } else {
+            let scalFactor = imageViewSize.width / imageSize.width
+            let height = imageSize.height * scalFactor
+            let topLeftY = (imageViewSize.height - height) * 0.5
+            return CGRect(x: 0, y: topLeftY, width: imageViewSize.width, height: height)
+        }
+    }
+}
---
+++ ImageCropper/cropAreaView.swift
@@ -0,0 +1,19 @@
+//
+//  cropAreaView.swift
+//  ImageCropper
+//
+//  Created by Jasmee Sengupta on 12/03/18.
+//  Copyright © 2018 Jasmee Sengupta. All rights reserved.
+//
+
+import UIKit
+
+class CropAreaView: UIView {
+    
+    override func point(inside point: CGPoint, with event:   UIEvent?) -> Bool {
+        
+        return false
+        
+    }
+    
+}
